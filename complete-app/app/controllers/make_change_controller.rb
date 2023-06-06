329ba45e54e97824cf2de8156434681b943dcd8d
Brent Halsey <brent.halsey@fusionauth.io>
2023-06-06 15:36:34 -0600

    round the pennies

--- complete-app/app/controllers/make_change_controller.rb
+++ complete-app/app/controllers/make_change_controller.rb
@@ -4,7 +4,7 @@ class MakeChangeController < ApplicationController
       amount = params[:amount].to_d
       @formatted_amount = sprintf("%0.2f", amount)
       @nickels = (amount / 0.05).to_i
-      @pennies = ((amount - 0.05*@nickels) / 0.01).to_i
+      @pennies = ((amount - 0.05*@nickels) / 0.01).round
     end
   end
 end
